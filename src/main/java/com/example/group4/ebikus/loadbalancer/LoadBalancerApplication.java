ea8291597728d1caf50ab247228a8601bf083862
mTemus <temus@wir.pl>
2018-07-17 17:13:58 +0200

    V1.0

---
+++ src/main/java/com/example/group4/ebikus/loadbalancer/LoadBalancerApplication.java
@@ -0,0 +1,14 @@
+package com.example.group4.ebikus.loadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class LoadBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalancerApplication.class, args);
+	}
+}
